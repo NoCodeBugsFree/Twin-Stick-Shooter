0ada72ef35b910017c6bc195e5c78a71f6d0925b
i5 <i5@I5-PC>
2017-06-09 21:46:17 +0300

    +

--- Source/TwinStickShooter/TwinStickShooter.Build.cs
+++ Source/TwinStickShooter/TwinStickShooter.Build.cs
@@ -8,9 +8,9 @@ public class TwinStickShooter : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "Slate", "SlateCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		//PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
